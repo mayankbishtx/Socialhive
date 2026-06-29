63c6f426ae41b90eb28c0d609e80297b5eb6881e
mayankbisht01 <mayankrajput2480@gmail.com>
2026-06-29 15:39:51 +0530

    add window native alert on logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -42,7 +42,13 @@ export default function Navbar() {
 
 
                 <button
-                    onClick={() => logout() }
+                    onClick={() => {
+                        const confirmed = window.confirm("Are you sure you want to logout?");
+                        if (confirmed) {
+                            logout();
+                            navigate("/login");
+                        }
+                    }}
                     className="px-2 py-2 border rounded-full shadow-lg cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
