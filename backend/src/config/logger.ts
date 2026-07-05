34f8036afa8a29799aa5949f2f4b9d5dc041809a
mayankbisht01 <mayankrajput2480@gmail.com>
2026-07-05 13:33:08 +0530

    fix logger for serverless deployment

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -1,21 +1,28 @@
 import winston from "winston";
 
+const transports: winston.transport[] = [
+  new winston.transports.Console()
+];
+
+if (process.env.NODE_ENV !== "production") {
+  transports.push(
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error"
+    }),
+    new winston.transports.File({
+      filename: "logs/combined.log"
+    })
+  );
+}
+
 const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
-    transports: [
-        new winston.transports.File({ filename: "log/error.log", level: "error" }),
-        new winston.transports.File({ filename: "logs/combined.log" }),
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        })
-    ]
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports
 });
 
 export default logger;
