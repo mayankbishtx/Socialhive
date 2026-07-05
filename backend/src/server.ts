66650c4e5ab1ec0fc5d68a9e002efd26faf518f1
mayankbisht01 <mayankrajput2480@gmail.com>
2026-07-05 12:52:38 +0530

    remove unnecessary variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,6 @@ const morganStream = {
 export const allowedOrigins = [
   "http://localhost", 
   "http://localhost:5173",
-  process.env.VERCEL_FRONTEND_URL,
   ].filter((origin): origin is string => Boolean(origin));
 
 app.use(helmet());
