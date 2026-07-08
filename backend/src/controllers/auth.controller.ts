8e3d1eb7061089beffadd530e2b73fa618e7bb0f
mayankbisht01 <mayankrajput2480@gmail.com>
2026-07-08 22:48:52 +0530

    fix: vercel issue

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -72,7 +72,7 @@ export const login = async (req: Request, res: Response) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: isProduction,
-             sameSite: isProduction ? "none" : "lax",
+             sameSite: "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
