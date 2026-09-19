80dd3d9c98c3694429158c8f3dfa8e4735a8ac6b
mayankbishtx <mayankrajput2480@gmail.com>
2026-09-19 16:22:04 +0530

    fix: remove depracated things

--- backend/src/validators/auth.validator.ts
+++ backend/src/validators/auth.validator.ts
@@ -4,12 +4,12 @@ import { Request, Response, NextFunction } from "express";
 const registrationSchema = z.object({
     name: z.string().min(2, "Name must be atleast 2 characters long"),
     username: z.string().min(3, "username must be atleast 3 characters").max(10, "Username too long").regex(/^[a-zA-Z0-9_]+$/,"Username can only contain letters, numbers, underscores"),
-    email: z.string().email("Invalid email address").toLowerCase(),
+    email: z.email("Invalid email address").toLowerCase(),
     password: z.string().min(6, "Password must be atleast 6 characters long"),
 })
 
 const loginSchema = z.object({
-    email: z.string().email("Invalid email address").toLowerCase(),
+    email: z.email("Invalid email address").toLowerCase(),
     password: z.string().min(6, "Password must be atleast 6 characters long"),
 })
 
